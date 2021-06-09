0ef1ea9f046a9e32fd0a1b0430100609c058150c
itspngu <34752364+itspngu@users.noreply.github.com>
2021-06-09 17:16:07 +0100

    fix: use commit types from @dgc-org/cz-conventional-changelog-for-jira

--- types.js
+++ types.js
@@ -1,19 +1,8 @@
-const types = [
-  "feat",
-	"fix",
-  "docs",
-  "refactor",
-  "test",
-  "style",
-	"build",
-	"ci",
-  "chore",
-	"perf",
-	"revert",
-];
+const typeInfo = require('@dgc-org/cz-conventional-changelog-for-jira/types');
+const types = Object.keys(typeInfo);
 
 module.exports.rules = {
-	"type-enum": [2, "always", types],
+  'type-enum': [2, 'always', types],
 };
 
 module.exports.value = () => types;
