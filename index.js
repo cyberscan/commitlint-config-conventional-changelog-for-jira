57fe6de47b91697709034e36103bb4bda36be0d8
itspngu <34752364+itspngu@users.noreply.github.com>
2021-06-10 20:32:50 +0100

    fix(JIRA): [ITCORE-90] force bump version

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ module.exports = {
   parserPreset: './parser-preset',
   // Rules defined here override or extend those from rulesets specified in extends: above
   rules: {
+    // Custom list of valid commit types
     'type-enum': typeEnum.rules['type-enum'],
+    // Require references to tickets
     'references-empty': [2, 'never'],
+    // Scopes should be upper case
+    'scope-case': [2, 'always', 'upper-case'],
   },
   // Inline plugin for additional rules
   // ref: https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-plugins.md#local-plugins
