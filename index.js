6d223f94356aab7d1563ab30eb403584a6d34460
itspngu <34752364+itspngu@users.noreply.github.com>
2021-06-13 08:49:49 +0100

    WIP: cleanup

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
-const types = require('./commit-types');
+const types = require("@dgc-org/cz-conventional-changelog-for-jira/commit-types");
 
 module.exports = {
-  // Pre-defined sets of rules to extend
-  extends: ['@commitlint/config-angular'],
-  // parserPreset is responsible for splitting commit messages into lexemes
-  // ref: https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-configuration.md#parser-presets
-  parserPreset: './parser-preset',
-  // Rules defined here override or extend those from rulesets specified in extends: above
-  rules: {
-    // Custom list of valid commit types (overrides @commitlint/config-angular)
-    'type-enum': types.rules['type-enum'],
-  },
-  // Functions that return true if commitlint should ignore the given message.
-  ignores: [
-    // Skip linting if commit message starts with "WIP:"
-    (msg) => msg.startsWith('WIP:'),
-    // Skip linting if env var COMMITLINT_DISABLE is set
-    () =>
-      process.env.COMMITLINT_DISABLE !== undefined &&
-      (process.env.COMMITLINT_DISABLE.toLowerCase() === 'true' ||
-        process.env.COMMITLINT_DISABLE === '1'),
-    // Skip linting if env var COMMITIZEN_DISABLE is set
-    () =>
-      process.env.COMMITIZEN_DISABLE !== undefined &&
-      (process.env.COMMITIZEN_DISABLE.toLowerCase() === 'true' ||
-        process.env.COMMITIZEN_DISABLE === '1'),
-  ],
-  // Help URL to display in error output
-  helpUrl:
-    'https://github.com/cyberscan/commitlint-config-conventional-changelog-for-jira#rules',
-  // Inline plugin for additional rules
-  // ref: https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-plugins.md#local-plugins
-  plugins: [
-    {
-      rules: {},
+    // Pre-defined sets of rules to extend
+    extends: ["@commitlint/config-angular"],
+    // parserPreset is responsible for splitting commit messages into lexemes
+    // ref: https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-configuration.md#parser-presets
+    parserPreset: {
+        parserOpts: {
+            // <type>(<scope>): <[ticket-id]> <subject>
+            headerPattern: /^(\w*)(\(\w+\))?:\s(\[[A-Z0-9]+-[0-9]+\])\s(.*)$/,
+            headerCorrespondence: ["type", "scope", "ticket", "subject"],
+            // TODO: load valid tickets from Jira
+            issuePrefixes: ["ITCORE-"],
+        },
     },
-  ],
+    // Rules defined here override or extend those from rulesets specified in extends: above
+    rules: {
+        // Custom list of valid commit types (overrides @commitlint/config-angular)
+        "type-enum": [2, "always", Object.keys(types)],
+    },
+    // Functions that return true if commitlint should ignore the given message.
+    ignores: [
+        // Skip linting if commit message starts with "WIP:"
+        (msg) => msg.startsWith("WIP:"),
+        // Skip linting if env var COMMITLINT_DISABLE is set
+        () =>
+            process.env.COMMITLINT_DISABLE !== undefined &&
+            (process.env.COMMITLINT_DISABLE.toLowerCase() === "true" ||
+                process.env.COMMITLINT_DISABLE === "1"),
+        // Skip linting if env var COMMITIZEN_DISABLE is set
+        () =>
+            process.env.COMMITIZEN_DISABLE !== undefined &&
+            (process.env.COMMITIZEN_DISABLE.toLowerCase() === "true" ||
+                process.env.COMMITIZEN_DISABLE === "1"),
+    ],
+    // Help URL to display in error output
+    helpUrl:
+        "https://github.com/cyberscan/commitlint-config-conventional-changelog-for-jira#rules",
+    // Inline plugin for additional rules
+    // ref: https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-plugins.md#local-plugins
+    plugins: [
+        {
+            rules: {},
+        },
+    ],
 };
--- parser-preset.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parserOpts: {
-    // <type>(<scope>): <[ticket-id]> <subject>
-    headerPattern: /^(\w*)(\(\w+\))?:\s(\[[A-Z0-9]+-[0-9]+\])\s(.*)$/,
-    headerCorrespondence: ['type', 'scope', 'ticket', 'subject'],
-    // TODO: load valid tickets from Jira
-    issuePrefixes: ['ITCORE-'],
-  },
-};
